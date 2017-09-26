77cd9a78c1015c0e60aeb8bd33f0162c1079ad94
dlmacedo <dlm@cin.ufpe.br>
2017-09-26 01:22:16 +0000

    Third commit...

--- train.sh
+++ train.sh
@@ -3,10 +3,10 @@
 
 
 
-#LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libgomp.so.1 python train.py \
-#--train_manifest data/mit_train_speaker_identification.csv --val_manifest data/mit_val_speaker_identification.csv --cuda --batch_size 20 \
-#--loss_type full --learning-rate 0.05 --learning_rate_decay_rate 0.2 --learning_rate_decay_epochs 500 500 --epochs 300 --hidden_layers 4 \
-#--kernel 11 --stride 2 --crop_begin 40 --crop_end 40 --sample_proportion 1.0 #| tee logs/full_sample1.0_crop40x40_conv11x2_arch400x4_lr0.05.txt
+LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libgomp.so.1 python train.py \
+--train_manifest data/mit_train_speaker_identification.csv --val_manifest data/mit_val_speaker_identification.csv --cuda --batch_size 20 \
+--loss_type full --learning-rate 0.05 --learning_rate_decay_rate 0.2 --learning_rate_decay_epochs 500 500 --epochs 300 --hidden_layers 4 \
+--kernel 11 --stride 2 --crop_begin 40 --crop_end 40 --sample_proportion 1.0 #| tee logs/full_sample1.0_crop40x40_conv11x2_arch400x4_lr0.05.txt
 
 
 
